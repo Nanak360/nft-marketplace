2f5997fb9b759bd8b187ee7c05a660247d6fb910
Nanak <nanakbandyopadhyay@hotmail.com>
2021-12-11 12:14:48 +0530

    add hardhat initial setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.PROJECT_ID}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.PROJECT_ID}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+  },
   solidity: "0.8.4",
 };
