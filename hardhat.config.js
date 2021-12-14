45a1122edbf7a7125444e7b10673fa289ecf74f7
Nanak <nanakbandyopadhyay@hotmail.com>
2021-12-14 09:47:43 +0530

    add front end logic  and styles

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
 
+const PROJECT_ID = "AY-WZ1IXvUc5DoSHQWBb-fSv68H2PwsQ";
+const WALLET_PRIVATE_KEY =
+  "500a743f70f9cf7251d91fbcf35bb4abb9a016e016fac57877f330c76ecd2ee5";
+
 module.exports = {
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337,
     },
     mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.PROJECT_ID}`,
-      accounts: [process.env.WALLET_PRIVATE_KEY],
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${PROJECT_ID}`,
+      accounts: [WALLET_PRIVATE_KEY],
     },
     mainnet: {
-      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.PROJECT_ID}`,
-      accounts: [process.env.WALLET_PRIVATE_KEY],
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${PROJECT_ID}`,
+      accounts: [WALLET_PRIVATE_KEY],
     },
   },
   solidity: "0.8.4",
